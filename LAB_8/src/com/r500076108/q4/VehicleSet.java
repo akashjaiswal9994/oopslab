a83aea8184cc89b7c08cb411783b05ab1ff13532
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2021-04-09 15:29:46 +0530

    updating code

--- LAB_8/src/com/r500076108/q4/VehicleSet.java
+++ LAB_8/src/com/r500076108/q4/VehicleSet.java
@@ -4,14 +4,17 @@ import java.util.Scanner;
 
 public class VehicleSet extends Exception{
     public VehicleSet(String D1,String D2){
-        super(D2);
+       super("done");
         if(D1.equals(D2)){
-          System.out.println("Collision Not Happens");
+            System.out.println("both ths Vehicle Going in "+D1+" direction");
+          System.out.println("So Collision will Not Happen");
 
         }
         else
         {
-            System.out.println("Vehicle collision Happens");
+            System.out.println("Vehicle A going from "+D1+ " to "+D2+" Direction ");
+            System.out.println("Vehicle B going from "+D2+ " to "+D1+" Direction ");
+            System.out.println("so Vehicle collision will  Happen");
         }
 
     }
@@ -20,9 +23,9 @@ public class VehicleSet extends Exception{
 class CheckIt{
     public static void main(String[] args){
         Scanner sc =new Scanner(System.in);
-        System.out.print("enter first direction = ");
+        System.out.print("Enter Vehicle A direction = ");
         String d1=sc.next().trim();
-        System.out.print("enter second direction = ");
+        System.out.print("Enter Vehicle B direction = ");
         String d2= sc.next().trim();
     try{
         throw new VehicleSet(d1,d2);
