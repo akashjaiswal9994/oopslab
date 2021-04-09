af1928f668dad875df0084841dbd163ed0b9e447
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2021-04-09 14:05:49 +0530

    update lab 7 file

--- LAB_7/src/com/r177219015/q1/TestInt.java
+++ LAB_7/src/com/r177219015/q1/TestInt.java
@@ -2,11 +2,9 @@ package com.r177219015.q1;
 
 import java.util.Scanner;
 
-//import com.r177219015.q1.Arithmethic;
+
 public class TestInt extends Arithmethic{
-    public static void main(String args[]){
-//        Arithmethic obj =new Arithmethic();
-//        obj.square();
+    public static void main(String[] args){
         TestInt obj1= new TestInt();
         Scanner sc= new Scanner(System.in);
         System.out.print("Enter the number = ");
--- LAB_7/src/com/r177219015/q3/Testing.java
+++ LAB_7/src/com/r177219015/q3/Testing.java
@@ -3,6 +3,6 @@ package com.r177219015.q3;
 public class Testing implements AA{
     public static void main(String args[]){
         Testing obj=new Testing();
-        //obj.x=6;
+        System.out.println(obj.x);
     }
 }
