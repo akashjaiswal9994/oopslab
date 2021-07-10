577a2ba19495a1066c24ece06139a80b4d0730b6
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2021-07-11 00:52:30 +0530

    update

---
+++ lab_5/src/com/r177219015/q3/Class2.java
@@ -0,0 +1,9 @@
+package com.r177219015.q3;
+
+public class Class2 extends Class1{
+    public static void main(String args[]){
+        Class2 cc =new Class2();
+        cc.displayStrings();
+
+    }
+}
