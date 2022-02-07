7e4d641900a60aec2082d81fe65ed36f265dcecf
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-02-07 19:16:25 +0530

    daily

--- competative programing/src/com/akash/cp/Q389.java
+++ competative programing/src/com/akash/cp/Q389.java
@@ -1,7 +1,23 @@
 package com.akash.cp;
 
+import java.util.HashSet;
+import java.util.Iterator;
+
 public class Q389 {
     public static void main(String[] args ){
+     String arr1= "abcd";
+     String  arr2="abcde";
+     char c=0;
+     HashSet<Character> cc1=new HashSet<Character>();
+     for(char c1:arr1.toCharArray()){
+        c^=c1;
+     }
+        HashSet<Character> cc2=new HashSet<Character>();
+        for(char c2:arr2.toCharArray()){
+            c^=c2;
+        }
+        System.out.println(c);
+
 
     }
 }
