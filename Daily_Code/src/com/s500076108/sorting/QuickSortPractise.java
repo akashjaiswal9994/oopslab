85a44c6b15a57ea36cb59206b40b1a3568a4bdee
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-07-21 20:49:52 +0530

    daily code

--- Daily_Code/src/com/s500076108/sorting/QuickSortPractise.java
+++ Daily_Code/src/com/s500076108/sorting/QuickSortPractise.java
@@ -1,4 +1,54 @@
 package com.s500076108.sorting;
 
+import java.util.Random;
+
 public class QuickSortPractise {
+    private static void printArray(int[] number) {
+        for(int i=0;i<number.length;i++){
+            System.out.print(number[i]+" ");
+        }
+    }
+    public static void main(String args[]){
+        Random random =new Random();
+        int[] number=new int[10];
+        int size=number.length;
+        for(int i=0;i<number.length;i++){
+            number[i]=random.nextInt(100);
+        }
+        System.out.println("Before sorting:");
+        printArray(number);
+        quickSort(number,0,size-1);
+        System.out.println("\nAfter sorting: ");
+        printArray(number);
+
+    }
+
+    private static void quickSort(int[] number, int li, int hi) {
+        if(li>=hi){
+            return;
+        }
+        int pivot=number[hi];
+        int leftIndex=li;
+        int rightIndex=hi;
+        while(leftIndex<rightIndex){
+            while (number[leftIndex]<=pivot && leftIndex<rightIndex){
+                leftIndex++;
+            }
+            while(number[rightIndex]>=pivot && leftIndex<rightIndex){
+                rightIndex--;
+            }
+            swap(number,leftIndex,rightIndex);
+        }
+        swap(number,leftIndex,hi);
+        quickSort(number,li,leftIndex-1);
+        quickSort(number,leftIndex+1,hi);
+    }
+
+    private static void swap(int[] number, int leftIndex, int rightIndex) {
+        int temp=number[leftIndex];
+        number[leftIndex]=number[rightIndex];
+        number[rightIndex]=temp;
+    }
+
+
 }
