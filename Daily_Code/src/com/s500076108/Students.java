be9ff1a808c069f8b01ab4af5f91bd16a728cc73
akashjaiswal9994 <jaiswalakash99.94@gmail.com>
2022-09-22 23:48:44 +0530

    sonnet

---
+++ Daily_Code/src/com/s500076108/Students.java
@@ -0,0 +1,39 @@
+package com.s500076108;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Students{
+    public static void getMarks( int[] marks){
+        Arrays.sort(marks);
+        for(int i=0;i<marks.length; i++){
+            if(marks[i]>100){
+                System.out.println("Invalid Marks");
+            }
+            else if(marks[i]>=40 && marks[i]<=50){
+                System.out.println(marks[i]+ "  Pass");
+            }
+            else if(marks[i]>=51 && marks[i]<=75){
+                System.out.println(marks[i]+ "  Merit");
+            }
+            else if(marks[i]>75){
+                System.out.println(marks[i]+ "  DISTINCTION");
+            }
+            else if(marks[i]<40){
+                System.out.println(marks[i]+ "  Fail");
+            }
+        }
+        
+    }
+
+    public static void main(String[] args){
+      Scanner sc = new Scanner(System.in);
+      int [] marks=new int[10];
+      for(int i=0;i<10;i++) {
+          System.out.print("Enter Marks Of Student "+ (i+1)+ ": ");
+          marks[i]= sc.nextInt();
+        }
+
+        getMarks(marks);
+    }
+}
